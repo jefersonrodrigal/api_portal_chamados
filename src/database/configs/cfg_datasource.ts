b0635f49f5240c882d7ebd7efb1c53c348e34093
jefersonrodrigal <jefersonrodrigoalmeida@outlook.com>
2023-10-17 01:16:58 -0300

    update configs do arquivo cfg_datasource.ts adicionando o dialeto postgres

--- src/database/configs/cfg_datasource.ts
+++ src/database/configs/cfg_datasource.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { DataSource } from 'typeorm'
 import { User } from '../entyties/User'
 import { Attendant } from '../entyties/Attendants'
@@ -9,7 +10,28 @@ import { createTableAtendentes1688403130973 } from '../migrations/1688403130973-
 import { createRelationshipUsuarios1Nchamados1688449083118 } from '../migrations/1688449083118-create-relationship-usuarios1-nchamados'
 import { createRelationshipAtendentes1Nchamados1688563727576 } from '../migrations/1688563727576-create-relationship-atendentes1-nchamados'
 
+
 export const ApiDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_USERPASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Attendant, Reports],
+  migrations: [
+    createDatabase1688071208975,
+    CreateTableUsers1688003742204,
+    createTableReports1688074400750,
+    createTableAtendentes1688403130973,
+    createRelationshipUsuarios1Nchamados1688449083118,
+    createRelationshipAtendentes1Nchamados1688563727576,
+  ],
+})
+
+export const dbTestApiDataSource = new DataSource({
   type: 'sqlite',
   database: './homologdb.sqlite',
   synchronize: true,
--- src/database/migrations/1688071208975-create_database.ts
+++ src/database/migrations/1688071208975-create_database.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config'
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createDatabase1688071208975 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createDatabase("portalDb.sql", true);
+    await queryRunner.createDatabase(process.env.DB_NAME, true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropDatabase("portalDb.sql", true);
+    await queryRunner.dropDatabase(process.env.DB_NAME, true);
   }
 }
